a26a5fcb7a344c7fda4a48059db2afc4d64e7e3e
Haley Tibbitts <117307728+haleytibbitts@users.noreply.github.com>
2023-02-11 16:57:56 -0500

    created component for saved playlists, added temp variables, small changes to css and Playlist.js

--- src/components/GenreApi.js
+++ src/components/GenreApi.js
@@ -1,55 +1,48 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Playlist from './Playlist';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Playlist from "./Playlist";
 
- const GenreApi = () => {
-    const [genre, setGenre] = useState('');
-   
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    
-    useEffect(() => {
-        const fetchGenre = async () => {
-        setLoading(true);
-        try {
-            const response = await axios.get('https://listen-api.listennotes.com/api/v2/genres', {
-                headers: {
-                    "X-ListenAPI-Key": '9a92e5d7fee14b86ad0d4bf6c532f35e',
-                },
-                params: {
-                    top_level_only: 1,
-                }
-            });
-            // console.log(response.data);
-            setGenre(response.data);
-        } catch (err) {
-            setError(err);
-        } finally {
-            setLoading(false);
-        }
-        };
-        fetchGenre();
-    }, []);
+const GenreApi = () => {
+  const [genre, setGenre] = useState("");
 
-        console.log (genre);
-    
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>An error occurred: {error.message}</p>;
+  useEffect(() => {
+    const fetchGenre = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "https://listen-api.listennotes.com/api/v2/genres",
+          {
+            headers: {
+              "X-ListenAPI-Key": "9a92e5d7fee14b86ad0d4bf6c532f35e",
+            },
+            params: {
+              top_level_only: 1,
+            },
+          }
+        );
+        // console.log(response.data);
+        setGenre(response.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchGenre();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>An error occurred: {error.message}</p>;
 
   return (
     <div>
-        {/* Pass the Genre list to the Select drop down list of genres */}
-        {/* <selectMenu data={genre} /> */}
+      {/* Pass the Genre list to the Select drop down list of genres */}
+      {/* <selectMenu data={genre} /> */}
     </div>
-  )
-
-}
+  );
+};
 
 export default GenreApi;
-
-
-
-
-
-

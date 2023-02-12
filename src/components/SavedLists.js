a742df2ebd5a102e4b1cad012e55714385bca1f7
Haley Tibbitts <117307728+haleytibbitts@users.noreply.github.com>
2023-02-12 14:24:11 -0500

    added scroll to top button component

--- src/components/SavedLists.js
+++ src/components/SavedLists.js
@@ -1,6 +1,7 @@
 import Playlist from "./Playlist";
+import ScrollToTop from "./ScrollToTop";
 
-const SavedLists = ({ savedPlaylists, newPlaylist, formValues }) => {
+const SavedLists = ({ savedPlaylists, newPlaylist, formValues, headerRef }) => {
   return (
     <section className="savedLists">
       <h2>Your Saved Playlists</h2>
@@ -14,6 +15,7 @@ const SavedLists = ({ savedPlaylists, newPlaylist, formValues }) => {
             );
           })}
       </div>
+      <ScrollToTop headerRef={headerRef} />
     </section>
   );
 };

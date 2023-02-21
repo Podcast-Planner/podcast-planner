5b984e6a4dac6b794c68ba3ea805e6e72c88cd9c
Renata Reinartz <rcreinartz@gmail.com>
2023-02-21 13:51:00 -0700

    double quotes about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 import { ArrowLeft, Envelope, GithubLogo, LinkedinLogo } from 'phosphor-react';
-import bioOne from'../assets/bioOne.jpeg';
-import bioTwo from '../assets/bioTwo.jpeg';
-import bioThree from '../assets/bioThree.jpeg';
-import bioFour from '../assets/bioFour.jpeg';
+import bioOne from "../assets/bioOne.jpeg";
+import bioTwo from "../assets/bioTwo.jpeg";
+import bioThree from "../assets/bioThree.jpeg";
+import bioFour from "../assets/bioFour.jpeg";
 import { useNavigate } from 'react-router-dom';
 
 const About = () => {
@@ -41,7 +41,7 @@ const About = () => {
                                     <li className='bio'>As a Web Developer, I strive to craft digital experiences that are not only technically sound, but also intuitive for users. I believe that a human-centered approach is key to creating truly impactful and delightful products. I am passionate about designing with the needs of the user in mind and am eager to be a part of a team that shares this philosophy and continuously pushes the boundaries of web development.</li>
                             <div className="socialIcons">
                                 <div className="aboutIcon">
-                                    <a href='https://www.linkedin.com/in/scott-cheung/' aria-label="LinkedIn profile" target="_blank" rel="noreferrer"><LinkedinLogo size={32} /></a>
+                                    <a href="https://www.linkedin.com/in/scott-cheung/" aria-label="LinkedIn profile" target="_blank" rel="noreferrer"><LinkedinLogo size={32} /></a>
                                 </div>
                                 <div className="aboutIcon">
                                     <a href="https://github.com/luckyNoodles" aria-label="Github profile" target="_blank" rel="noreferrer"><GithubLogo size={32} /></a>
@@ -60,13 +60,13 @@ const About = () => {
                                     <li className='bio'>Hi, 👋 I'm Renata, I'm a front-end developer. Providing end users with clean, well-written, SEO-friendly code is one of my greatest passions. Besides front-end development, I am a CPA, mother, 🧘‍♀️ yoga teacher, travel enthusiast, and advocate for children with special needs. For me, being able to provide accessible websites is also very meaningful. I'd ❤️ to hear from you click on my link below.</li>
                             <div className="socialIcons">
                                 <div className="aboutIcon">
-                                    <a href='https://www.linkedin.com/in/renata1026/' aria-label="LinkedIn profile" target="_blank" rel="noreferrer"><LinkedinLogo size={32} /></a>
+                                    <a href="https://www.linkedin.com/in/renata1026/" aria-label="LinkedIn profile" target="_blank" rel="noreferrer"><LinkedinLogo size={32} /></a>
                                 </div>
                                 <div className="aboutIcon">
-                                    <a href='https://github.com/renata1026' aria-label="Github profile" target="_blank" rel="noreferrer"><GithubLogo size={32} /></a>
+                                    <a href="https://github.com/renata1026" aria-label="Github profile" target="_blank" rel="noreferrer"><GithubLogo size={32} /></a>
                                 </div>
                                 <div className="aboutIcon">
-                                    <a href='mailto:renata.codes.dev@gmail.com' aria-label="Email" target="_blank" rel="noreferrer"><Envelope size={32} /></a>
+                                    <a href="mailto:renata.codes.dev@gmail.com" aria-label="Email" target="_blank" rel="noreferrer"><Envelope size={32} /></a>
                                 </div>
                             </div>
                         </ul>
@@ -79,7 +79,7 @@ const About = () => {
                             <li className="bio">Hello, I’m Richard. I am a front-end developer with experience building applications with JavaScript (ES6+), HTML5, and CSS3. With a background in Mechanical Engineering, I have developed and demonstrated excellent continuous improvement skills and the ability to understand and analyze complex mechanisms.</li>
                             <div className="socialIcons">
                                 <div className="aboutIcon">
-                                    <a href='https://www.linkedin.com/in/richard-tillo/' aria-label="LinkedIn Profile" target="_blank" rel="noreferrer"><LinkedinLogo size={32} /></a>
+                                    <a href="https://www.linkedin.com/in/richard-tillo/" aria-label="LinkedIn Profile" target="_blank" rel="noreferrer"><LinkedinLogo size={32} /></a>
                                 </div>
                                 <div className="aboutIcon">
                                     <a href='https://github.com/richardJET' aria-label="Github Profile" target="_blank" rel="noreferrer"><GithubLogo size={32} /></a>

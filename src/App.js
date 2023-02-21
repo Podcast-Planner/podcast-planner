d2b526cbe4fa36420a68ba19ad5e9f409d9b710a
scottCheung <scottcx2@gmail.com>
2023-02-20 20:20:59 -0500

    moved about from pages to component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Routes, Route } from "react-router-dom";
 
 // Components
 import SavedLists from "./components/SavedLists";
-import About from "./pages/About"
+import About from "./components/About"
 import Form from "./components/Form";
 import Results from "./components/Results";
 import Header from "./components/Header";
